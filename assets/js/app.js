71b218dd2b96ac1a2298c802e813ba8f20a36942
ughurba <ughurba@edu.code.az>
2022-05-12 16:07:54 +0400

    create app js

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+const tabsBtn = document.querySelectorAll('.nav-item');
+const tabsItems = document.querySelectorAll('.blog-sub-item')
+
+tabsBtn.forEach(function(item){
+
+    item.addEventListener('click',function(){
+
+      let  currentBtn = item;
+      
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+        
+        tabsItems.forEach(function(item){
+            item.classList.remove("show");
+        })
+
+        tabsBtn.forEach(function(item){
+            item.classList.remove("show");
+        });
+      
+     
+
+        currentTab.classList.add('show');
+        
+    })
+
+
+})
